e8241f2b7fc13b595a0f7d9dc22b31901ecb378b
Nishant <154457095+whynishant@users.noreply.github.com>
2024-12-11 04:35:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,36 +4,32 @@ from flask_socketio import SocketIO, join_room, leave_room, emit
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 @socketio.on('join')
 def on_join(data):
     username = data['username']
     room = data['room']
     join_room(room)
-    emit('message', {'msg': f"{username} has joined the room {room}!"}, to=room)
-
-
-@socketio.on('player-state-change')
-def on_player_state_change(data):
-    room = data['room']
-    action = data['action']
-
-    # Emit player state change to all users in the room
-    emit('player-state-change', {'action': action, 'room': room}, to=room)
-
+    emit('message', f"{username} has joined the room {room}!", room=room)
 
 @socketio.on('leave')
 def on_leave(data):
     username = data['username']
     room = data['room']
     leave_room(room)
-    emit('message', {'msg': f"{username} has left the room {room}!"}, to=room)
+    emit('message', f"{username} has left the room {room}!", room=room)
 
+@socketio.on('sync-player')
+def sync_player(data):
+    action = data['action']
+    song = data.get('song', '')
+    room = data['room']  # Make sure you send the room data from frontend
+
+    # Emit the sync event to all clients in the room
+    emit('sync-player', {'action': action, 'song': song}, room=room)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, port=5003)
+    socketio.run(app, debug=True)
